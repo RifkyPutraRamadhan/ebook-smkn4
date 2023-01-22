7e48d74f1ae7b3f01685342544e87b4d35f760a2
Rifky Putra Ramadhan <105836756+RifkyPutraRamadhan@users.noreply.github.com>
2023-01-22 13:52:11 +0700

    Update Dashboard.vue

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -2,42 +2,42 @@
   <body>
   <h2 align="center" class="mb-4">Silahkan Pilih Kompetensi Keahlian Terlebih Dahulu</h2>
    <div class="container mb-3">
-    <div class="card">
+    <div class="card" style="height: 12rem;">
   <h5 class="card-header">TJKT</h5>
   <div class="card-body b-tjkt">
-    <h5 class="card-title">TEKNIK JARINGAN KOMPUTER DAN TELEKOMUNIKASI</h5>
+    <h5 class="text-white card-title mb-5">TEKNIK JARINGAN KOMPUTER DAN TELEKOMUNIKASI</h5>
     <router-link to="/tjkt" class="btn btn-primary">Pilih Opsi</router-link>
   </div>
   </div>
   <br>
-  <div class="card">
+  <div class="card" style="height: 12rem;">
   <h5 class="card-header">TBSM</h5>
   <div class="card-body b-tbsm">
-    <h5 class="card-title">TEKNIK DAN BISNIS SEPEDA MOTOR</h5>
+    <h5 class="text-white card-title mb-5">TEKNIK DAN BISNIS SEPEDA MOTOR</h5>
     <router-link to="/tbsm" class="btn btn-primary">Pilih Opsi</router-link>
   </div>
 </div>
 <br>
-<div class="card">
+<div class="card" style="height: 12rem;">
   <h5 class="card-header">PPLG</h5>
   <div class="card-body b-rpl">
-    <h5 class="card-title">PENGEMBANGAN PERANGKAT LUNAK DAN GAME</h5>
+    <h5 class="text-white card-title mb-5">PENGEMBANGAN PERANGKAT LUNAK DAN GAME</h5>
     <router-link to="/pplg" class="btn btn-primary">Pilih Opsi</router-link>
   </div>
 </div>
 <br>
-<div class="card">
+<div class="card" style="height: 12rem;">
   <h5 class="card-header">DKV</h5>
   <div class="card-body b-dkv">
-    <h5 class="card-title">DESAIN KOMUNIKASI VISUAL</h5>
+    <h5 class="text-white card-title mb-5">DESAIN KOMUNIKASI VISUAL</h5>
     <router-link to="/dkv" class="btn btn-primary">Pilih Opsi</router-link>
   </div>
 </div>
 <br>
-<div class="card">
+<div class="card" style="height: 12rem;">
   <h5 class="card-header">TOI</h5>
   <div class="card-body b-toi">
-    <h5 class="card-title">TEKNIK OTOMASI INDUSTRI</h5>
+    <h5 class="text-white card-title mb-5">TEKNIK OTOMASI INDUSTRI</h5>
     <router-link to="/toi" class="btn btn-primary">Pilih Opsi</router-link>
   </div>
   </div>
