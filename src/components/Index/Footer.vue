fe0934d5db44d61c4d2e02119b4fbdea79880957
Rifky Putra Ramadhan <105836756+RifkyPutraRamadhan@users.noreply.github.com>
2023-02-26 16:24:58 +0700

    Update Footer.vue

--- src/components/Index/Footer.vue
+++ src/components/Index/Footer.vue
@@ -5,7 +5,7 @@
     <footer class="footer mt-5 w-100">
       <div class="text-center text-white p-3" style="background-color: #1b1e23">
         © Copyright - SMK NEGERI 4 TASIKMALAYA 2023<br />Designed By
-        <strong class="text-white">RifkyPutra</strong>
+          <a class="text-primary"  href="https://github.com/RifkyPutraRamadhan" target="_blank">RifkyPutra</a>
       </div>
     </footer>
   </div>
