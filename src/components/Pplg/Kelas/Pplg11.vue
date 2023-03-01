632f6372a685393d43c4b4d482d7bf58b313ff82
Rifky Putra Ramadhan <105836756+RifkyPutraRamadhan@users.noreply.github.com>
2023-03-01 18:20:42 +0700

    Update Pplg11.vue

--- src/components/Pplg/Kelas/Pplg11.vue
+++ src/components/Pplg/Kelas/Pplg11.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-md-12">
         <h3 class="mb3">PPLG - Kelas 11</h3>
+        <div class="card-body table-responsive p-0">
         <table class="table table-bordered table-striped">
           <thead>
             <tr align="center">
@@ -121,6 +122,7 @@
              </tr>
           </tbody>
           </table>
+          </div>
           </div>
           </div>
           </div>
